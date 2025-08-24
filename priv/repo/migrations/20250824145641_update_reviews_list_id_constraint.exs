c18ccf91551e42c979b17b9f0d17e6c942685450
Clodoaldo Favaro <clodoaldo.favaro@grin.co>
2025-08-24 17:16:09 -0300

    fix: drop constraint

--- priv/repo/migrations/20250824145641_update_reviews_list_id_constraint.exs
+++ priv/repo/migrations/20250824145641_update_reviews_list_id_constraint.exs
@@ -4,7 +4,7 @@ defmodule LeetcodeSpaced.Repo.Migrations.UpdateReviewsListIdConstraint do
   def change do
     # Drop the existing foreign key constraint
     drop constraint(:reviews, "reviews_list_id_fkey")
-    
+
     # Add the new foreign key constraint with cascade delete
     alter table(:reviews) do
       modify :list_id, references(:lists, on_delete: :delete_all)
